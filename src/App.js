0af65833ca5ba8b988129db793c630e57da7fb0a
stephaniemlozano <stephaniesadowski@Stephanies-Air.home>
2023-03-20 22:03:36 -0400

    updating nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element='home'/>
+        {/* <Route path='/' element='home'/>
         <Route path='/aboutus' element='about us'/>
         <Route path='/services' element='services'/>
-        <Route path='/contact' element='contact'/>
+        <Route path='/contact' element='contact'/> */}
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
-import { Nav } from "react-bootstrap"
+import { Nav } from 'react-bootstrap'
+import logo from '../assets/images/lifeatpeace.png'
 
 const Header = () => {
   return (
-    <>
-    
-    </>
+    <div className='header'>
+      <img src={logo} alt="Life at Peace logo" />
+      <Nav className="justify-content-end" style={{ width: "100%" }} activeKey='/home'>
+        <Nav.Item>
+          <Nav.Link href='/home'>Home</Nav.Link>
+          <Nav.Link href='/aboutus'>About Us</Nav.Link>
+          <Nav.Link href='/services'>Services</Nav.Link>
+          <Nav.Link href='/contact'>Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
   )
 }
 
