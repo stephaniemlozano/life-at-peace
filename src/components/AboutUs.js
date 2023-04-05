ee446ba9c7022067c36c81babd3d5a0e79aed1ba
stephaniemlozano <stephaniesadowski@Stephanies-Air.home>
2023-04-04 21:59:26 -0400

    testing with scroll functionality

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,7 +2,7 @@ import headshot from '../assets/images/co.jpeg'
 
 const About = () => {
   return (
-    <div className="about">
+    <div className="about" id='about'>
       <img src={headshot} alt="Self Portrait of Carolina Onorati" />
       <p>Every great change starts with one decision to try something different and I applaud your courage in taking this first step! I am a Licensed Mental Health Counselor in Florida and began working with individuals and families in 2014. I have helped individuals and families work through some of life's most difficult moments. It is my belief that you are the expert of your life and the author of your story, I am simply here to provide support and guidance as you find the right words for each chapter.
       <br />
